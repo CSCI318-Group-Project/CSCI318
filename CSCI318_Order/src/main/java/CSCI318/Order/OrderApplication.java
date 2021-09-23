e06210e202c03a0cf75aca1c4019bdc601b4aecc
Turnbulll <71480607+Turnbulll@users.noreply.github.com>
2021-09-23 12:11:18 +1000

    Package Changes

---
+++ CSCI318_Order/src/main/java/CSCI318/Order/OrderApplication.java
@@ -0,0 +1,13 @@
+package CSCI318.Order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderApplication.class, args);
+	}
+
+}
